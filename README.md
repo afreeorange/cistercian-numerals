10b328c4b82041bd0fe4a34d5d44df36fcca6810
Nikhil Anand <mail@nikhil.io>
2021-07-17 07:55:42 -0500

    Rotate, README, etc

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # [Cistercian Numeral](https://en.wikipedia.org/wiki/Cistercian_numerals) Generator
+
+```bash
+# Install dependencies
+yarn
+
+# Live-reloading server
+yarn start
+
+# Build
+yarn build
+```
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,19 @@ const numberChunks = (n: number) => {
 
 const handleInput = ({ target: { value: n } }) => {
   if (!n || n === "") {
-    u("#numerals").html("Enter something...");
+    u("small").html("Enter something...");
+    u("#numerals").html("");
     return;
   }
 
   if (n < 1 || n > 9999) {
-    u("#numerals").html("Can't do that number...");
+    u("small").html("Can't do that number...");
+    u("#numerals").html("");
     return;
   }
 
   const chunks = numberChunks(n);
+  u("small").html("");
   u("#numerals").html(
     chunks.map((chunk) => `<div data-number="${chunk}"></div>`).join("")
   );
@@ -26,4 +29,10 @@ const handleInput = ({ target: { value: n } }) => {
   document
     .querySelector("input")
     .addEventListener("keyup", (e: any) => handleInput(e));
+
+  document
+    .querySelector("input[name='flipped']")
+    .addEventListener("change", () => {
+      u("#numerals").toggleClass("flipped");
+    });
 })();
